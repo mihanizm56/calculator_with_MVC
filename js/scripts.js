0177b1fdd4361333b076b429caa112549aeb37e4
mihanizm56 <m727507.56@mail.ru>
2018-09-11 01:13:26 +0300

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,54 @@
 const Model = {
-
+  a,
+  b,
+  sum(a,b){
+    return a+b
+  },
+  diff(a,b){
+    return a-b
+  },
+  mul(a,b){
+    return a*b
+  },
+  div(a,b){
+    if(b!==0){ 
+      return a/b
+    }
+    alert('введите делитель отличный от нуля!')
+    throw new Error()
+  },
+  insertNUmber(number){
+    return View.showNumber(number)
+  }
 }
 
 const View = {
-
+  showNumber(number){
+    const display = document.getElementById('display')
+    display.value = number
+  }
 }
 
 const Controller = {
-
+  delegateClick(event){
+    if(event.target.className = ''){}
+    if (event.target.className = ''){}
+    if(event.target.className = ''){}
+    if (event.target.className = ''){}
+    //console.log(event.target.value)
+    //Model.insertNUmber(event.target.value)
+  }
 }
+
+const Router = {
+  Listeners(whereToListen,callback){
+    whereToListen.addEventListener('click',()=>{
+      Controller.delegateClick(event)
+    })
+  }
+}
+
+const initialize = () => {
+  Router.Listeners(document)
+}
+initialize()
