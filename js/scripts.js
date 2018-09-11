bb225d3a39594451e4d3849c149d13d67f281c25
mihanizm56 <m727507.56@mail.ru>
2018-09-12 01:31:28 +0300

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,38 @@
 const Model = {
   equality(string){
     View.clearDisplay()
-    View.showItems(eval(string))
+    View.showItems(Math.round(eval(string)*10000)/10000)
   },
 
   addToListOfNumbers(value){
     View.showItems(value)
   },
-
-  clearResult(){
+  backwards(value){
+    console.log(value.slice(0,-1))
+    //View.showItems(value.slice(-1))
+  },
+  eqSquare(number){
+    View.clearDisplay()
+    View.showItems(number * number)
+  },
+  eqSqrt(number) {
+    View.clearDisplay()
+    if(number>=0){
+      View.showItems(Math.sqrt(number))
+      return
+    }
+    View.showItems(0)
+  },
+  clearResult() {
     View.clearDisplay()
   }
 }
 
 const View = {
   showItems(number) {
+    if(number=='Infinity'){
+      number = '0'
+    }
     document.getElementById('display').value += number
   },
 
@@ -35,14 +53,29 @@ const Controller = {
   },
 
   delegateClick(event){
+
     if (event.target.value){
-      Model.addToListOfNumbers(event.target.value)
+      if (event.target.value == '^2'){
+        Model.eqSquare(document.getElementById('display').value)
+        return
+      } 
+      if(event.target.value == 'sqrt') {
+        Model.eqSqrt(document.getElementById('display').value)
+        return
+      } 
+      if (event.target.value == 'back') {
+        Model.backwards(document.getElementById('display').value)
+        return
+      } 
+      Model.addToListOfNumbers(event.target.value) 
     }
-    else if (event.target.className == 'button button-cancel'){
+    if (event.target.className == 'button button-cancel'){
       Model.clearResult()
+      return
     }
-    else if (event.target.className == 'button button-eq') {
+    if (event.target.className == 'button button-eq') {
       Model.equality(document.getElementById('display').value)
+      return
     }
   }
 }
